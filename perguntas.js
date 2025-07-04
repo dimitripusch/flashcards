3768039c89528ee2a2f7e3456743905c40c71645
Dimitri Barcelos Pusch <dimitri.pusch@escola.pr.gov.br>
2025-07-04 09:28:26 -0300

    4.7.25

--- perguntas.js
+++ perguntas.js
@@ -1,9 +1,9 @@
-criarCartao("Química", "O que é entalpia?", "Entalpia é a quantidade total de energia de um sistema em forma de calor sob pressão constante.");
-criarCartao("Química", "O que representa a variação de entalpia (ΔH)?", "Representa a quantidade de calor liberada ou absorvida em uma reação química.");
-criarCartao("Química", "Quando ΔH é negativo, o que ocorre?", "Ocorre uma reação exotérmica, com liberação de calor.");
-criarCartao("Química", "Quando ΔH é positivo, o que significa?", "Significa que a reação é endotérmica, absorvendo calor do ambiente.");
-criarCartao("Química", "Como se calcula a variação de entalpia (ΔH)?", "ΔH = Hprodutos - Hreagentes");
-criarCartao("Química", "O que é uma reação exotérmica?", "É uma reação que libera calor para o meio ambiente, com ΔH negativo.");
-criarCartao("Química", "O que é uma reação endotérmica?", "É uma reação que absorve calor do meio ambiente, com ΔH positivo.");
-criarCartao("Química", "O que é entalpia de formação?", "É a variação de entalpia na formação de 1 mol de substância a partir de seus elementos no estado padrão.");
-criarCartao("Química", "Qual a unidade usual da entalpia?", "Quilojoule por mol (kJ/mol).");
+criaCartao("Química", "O que é entalpia?", "Entalpia é a quantidade total de energia de um sistema em forma de calor sob pressão constante.");
+criaCartao("Química", "O que representa a variação de entalpia (ΔH)?", "Representa a quantidade de calor liberada ou absorvida em uma reação química.");
+criaCartao("Química", "Quando ΔH é negativo, o que ocorre?", "Ocorre uma reação exotérmica, com liberação de calor.");
+criaCartao("Química", "Quando ΔH é positivo, o que significa?", "Significa que a reação é endotérmica, absorvendo calor do ambiente.");
+criaCartao("Química", "Como se calcula a variação de entalpia (ΔH)?", "ΔH = Hprodutos - Hreagentes");
+criaCartao("Química", "O que é uma reação exotérmica?", "É uma reação que libera calor para o meio ambiente, com ΔH negativo.");
+criaCartao("Química", "O que é uma reação endotérmica?", "É uma reação que absorve calor do meio ambiente, com ΔH positivo.");
+criaCartao("Química", "O que é entalpia de formação?", "É a variação de entalpia na formação de 1 mol de substância a partir de seus elementos no estado padrão.");
+criaCartao("Química", "Qual a unidade usual da entalpia?", "Quilojoule por mol (kJ/mol).");
